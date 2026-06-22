a692e8945bda02ced9f6f14e6aabfdb1f2f7d9f5
user230217 <abdulhamidkilic05@gmail.com>
2026-06-22 14:10:09 +0200

    removed margin to regain space and added domain for rpm, pitch and roll

--- fahrsimulator-main/react_app/src/components/widgets/silab/SilabSignalChart.jsx
+++ fahrsimulator-main/react_app/src/components/widgets/silab/SilabSignalChart.jsx
@@ -93,7 +93,7 @@ const SilabSignalChart = memo(function SilabSignalChart({ signal }) {
           </span>
         </div>
         <ResponsiveContainer width="100%" height="100%">
-          <LineChart data={data} margin={{ top: 10, right: 12, left: signal.leftMargin, bottom: 10 }}>
+          <LineChart data={data} margin={{ top: 10, right: 12, left: 0, bottom: 0 }}>
             <CartesianGrid strokeDasharray="3 3" stroke="#dbe5ef" />
             <XAxis
               dataKey="elapsedMs"
@@ -102,13 +102,14 @@ const SilabSignalChart = memo(function SilabSignalChart({ signal }) {
               allowDataOverflow
               stroke="#5a6b7a"
               tick={{ fontSize: 11 }}
+              height={18}
+              tickMargin={3}
               tickFormatter={(val) => formatElapsedSeconds(val / 1000)}
             />
             <YAxis
               stroke="#5a6b7a"
               tick={{ fontSize: 11 }}
               width={signal.yAxisWidth}
-              unit={signal.unit && !signal.tickFormatter ? signal.unit : undefined}
               domain={signal.domain}
               ticks={signal.ticks}
               interval={signal.ticks ? 0 : undefined}
